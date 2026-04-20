8ef2acf3e411d8626860f4e0cbc8b18eb1d00d4d
IHN TOPUP <ihntopup@gmail.com>
2026-04-20 12:45:24 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8707900714-4ac86",
+  "appId": "1:264048901388:web:615ffe37cab51b2aa15d29",
+  "apiKey": "AIzaSyAEV83HDhHNYZF2O8BuUDMaMONk7XA7fUM",
+  "authDomain": "studio-8707900714-4ac86.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "264048901388"
+};
