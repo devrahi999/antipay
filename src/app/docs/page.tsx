075bafea9413998babb0270c063b075b4ee93bd6
IHN TOPUP <ihntopup@gmail.com>
2026-04-21 17:42:34 +0000

    Try fixing this error: `Runtime ReferenceError: Badge is not defined. Er

--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -1,8 +1,8 @@
-
 'use client';
 
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
+import { Badge } from "@/components/ui/badge"
 import { 
   ShieldCheck, 
   ArrowLeft, 
